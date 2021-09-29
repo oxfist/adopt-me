70153d8cc5299333256e3b155616844f11043063
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-09-28 23:50:41 -0300

    Add animal and breed search params

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = Object.freeze(["bird", "cat", "dog", "rabbit", "reptile"]);
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Santiago, Chile");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -10,10 +15,42 @@ const SearchParams = () => {
           Location
           <input
             id="location"
-            onChange={(event) => setLocation(event.target.value)}
+            onChange={(e) => setLocation(e.target.value)}
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="name"
+            name={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="name"
+            name={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
