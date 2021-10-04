82d78d62c9b5de784c2e461f4cd4d0f62728213e
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-10-03 23:46:24 -0300

    Add React Router and two base routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./Details";
 
 import SearchParams from "./SearchParams";
 
@@ -7,7 +9,16 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
